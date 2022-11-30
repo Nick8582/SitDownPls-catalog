45a43edb87783bfc35144d7643a118134f6f8515
Potapov Vladislav <89172111013q@gmail.com>
2022-12-01 01:55:08 +0300

    Стилизация компонента выюора категории

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,10 @@ new Choices(reg, {
   shouldSort: !1,
   classNames: {containerOuter: "choices choices__region", list: "choices__list--region", flippedState: ""}
 });
+let fil = document.querySelector(".header__filter");
+new Choices(fil, {
+  searchEnabled: !1,
+  itemSelectText: "",
+  shouldSort: !1,
+  classNames: {containerOuter:"choices choices__filter",list:"choices__list--filter",flippedState:""}
+});
